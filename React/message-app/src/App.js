80ad8272eba3153a0f73ad4d49c3c50c7f4c1a4a
Ravi Lamontagne <lamontagne.ravi@gmail.com>
2018-12-27 07:31:35 -0500

    Initial commit

---
+++ React/message-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import MessageView from './messages/message-view';
+
+class App extends Component {
+	render(){
+		return (<MessageView />)
+	}
+}
+
+export default App;
---
+++ React/message-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+//const user = {firstName: "Jane", lastName: "Doe"} 
+//const element = <p> Hello, {user.firstName} {user.lastName} </p>;
+
+ReactDom.render(<App/>, document.getElementById('root'));
