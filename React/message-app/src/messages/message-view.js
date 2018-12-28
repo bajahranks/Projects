0d803a4bced837dad0cfae955d065061da970442
Ravi Lamontagne <lamontagne.ravi@gmail.com>
2018-12-28 07:47:46 -0500

    Dynamic Messages
    Stateful Component

--- React/message-app/src/messages/message-view.js
+++ React/message-app/src/messages/message-view.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MessageView extends Component {
 	render() {
+		const message = this.props.message;
+
 		return(
 			<div className="container">
 				<div className="from">
 					<span className="label">From: </span>
-					<span className="value">John Doe</span>
+					<span className="value">{message.from}</span>
 				</div>
 				<div className="status">
 					<span className="label">Status: </span>
-					<span className="value"> Unread</span>
+					<span 
+className="value">{message.status}</span>
 				</div>
 				<div className="message">
 					<span className="label">Message: </span>
-					<span className="value">Have a great day!</span>
+					<span className="value">{message.content}</span>
 				</div>
 			</div>
 		) 
 	}
+	// Mark message input parameter as required
+	static propTypes = { message: PropTypes.object.isRequired }
 }
 
 export default MessageView;
